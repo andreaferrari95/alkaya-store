9f890c155ded886788526018799ecd82bef65f9f
Andrea Ferrari | Phonetastik <131166715+andreaferrari95@users.noreply.github.com>
2025-04-17 10:40:24 +0200

    asd

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@ import { ReadonlyURLSearchParams } from "next/navigation";
 
 export const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : "http://localhost:3000";
+  : "http://localhost:3001";
 
 export const createUrl = (
   pathname: string,
